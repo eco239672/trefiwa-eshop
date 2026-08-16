257edd3a3f5187cc1c79f61eff01b40880791655
eco239672 <eco239672@gmail.com>
2026-08-16 07:59:26 +0200

    doplnkovy sortiment

--- app/doplnkovy sortiment/page.tsx
+++ app/doplnkovy sortiment/page.tsx
@@ -152,6 +152,7 @@ export default function HealthyFoodsCategory() {
               <li><Link href="/zdrave-potraviny" className="hover:text-white transition-colors">Zdravé potraviny</Link></li>
               <li><Link href="/susene-ovocie" className="hover:text-white transition-colors">Sušené ovocie</Link></li>
               <li><Link href="/doplnkovy-sortiment" className="hover:text-white transition-colors">Doplnkový sortiment</Link></li>
+              <li><Link href="/doplnkovy-sortiment" className="hover:text-white transition-colors">Eko sortiment</Link></li>
             </ul>
           </div>
           <div>
