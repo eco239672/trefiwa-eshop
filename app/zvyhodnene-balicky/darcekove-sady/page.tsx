a5c5b72cafd2c259d9e004a0b197b94450ba6288
eco239672 <eco239672@gmail.com>
2026-08-23 20:31:50 +0200

    Pridane sekcie

--- app/zvyhodnene-balicky/darcekove-sady/page.tsx
+++ app/zvyhodnene-balicky/darcekove-sady/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import { getProductsBySubCategory } from "../actions";
+import { getProductsBySubCategory } from "../../actions";
 
 type Product = {
   id: string;
